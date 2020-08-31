fa668819084baac4a3a04aa7105660d870a61364
yceethetechie <waplurd1@gmail.com>
2020-08-31 14:26:10 +0500

    created signup actions, db and wrote to db from signup page

--- src/Store/Actions/authActions.js
+++ src/Store/Actions/authActions.js
@@ -17,23 +17,23 @@ export const signOut = () => {
     }
 }
 
-export const signUp = (newUser) => {
-    return (dispatch,getState, {getFirebase,getFirestore}) => {
-        const firebase = getFirebase();
-        const firestore = getFirestore();
-        firebase.auth().createUserWithEmailAndPassword(
-            newUser.email,
-            newUser.password
+// export const signUp = (newUser) => {
+//     return (dispatch,getState, {getFirebase,getFirestore}) => {
+//         const firebase = getFirebase();
+//         const firestore = getFirestore();
+//         firebase.auth().createUserWithEmailAndPassword(
+//             newUser.email,
+//             newUser.password
 
-        ).then((resp)=> {
-            return firestore.collection('users').doc(resp.user.uid).set({
-                firstName : newUser.firstName,
-                   lastName : newUser.lastName,
-                initials : newUser.firstName[0] + newUser.lastName[0]
-            })
-        })
-        .then(()=> { dispatch({type: 'SIGNUP_SUCCESS'})})
-        .catch((err)=> {dispatch ({type: 'SIGNUP_ERROR' ,err}) })
-    }
+//         ).then((resp)=> {
+//             return firestore.collection('users').doc(resp.user.uid).set({
+//                 firstName : newUser.firstName,
+//                    lastName : newUser.lastName,
+//                 initials : newUser.firstName[0] + newUser.lastName[0]
+//             })
+//         })
+//         .then(()=> { dispatch({type: 'SIGNUP_SUCCESS'})})
+//         .catch((err)=> {dispatch ({type: 'SIGNUP_ERROR' ,err}) })
+//     }
 
-}   
+// }   
